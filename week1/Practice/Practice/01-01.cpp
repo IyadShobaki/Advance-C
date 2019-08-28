80a5f7ffbe5c0b84509a28aea97647143192aeef
Iyad Shobaki <iyadshobaki82@gmail.com>
2019-08-28 19:36:25 -0400

    first commit

---
+++ week1/Practice/Practice/01-01.cpp
@@ -0,0 +1,20 @@
+//precompiled header file was called "stdafx.h" in 2017 and earliar versions, now its called "pch.h"
+//#include "pch.h"
+#include <iostream>
+using namespace std;
+//int main() {
+//	int numberOgLanguages;
+//	cout << "Hello reader.\n"
+//		<< "Welcome to c++.\n";
+//
+//	cout << "How many programming languages have you used? ";
+//	cin >> numberOgLanguages;
+//
+//	if (numberOgLanguages < 1)
+//		cout << "Read the preface. You may prefer\n"
+//		<< "a more elementry book by the same author.\n";
+//	else
+//		cout << "Enjoy the book.\n";
+//
+//	return 0;
+//}
---
+++ week1/Practice/Practice/StudentAverage-w1a.cpp
@@ -0,0 +1,73 @@
+/*--Iyad Shobaki
+	Fall 2019 ADVANCED C++ PROGRAMMING - CSE234
+	Professor  Steve Morosko
+	Week 1 Lab (Chapter 1-5) - PART 1 (Student Average)
+	8 / 27 / 2019 */
+
+#include <iostream>
+using namespace std;
+//// Function takes an array as an arg
+//double qualityPoints(int arr[]);
+//// Function takes double number as an arg
+//double qualityPoints(double average);
+//int main() {
+//	cout.setf(ios::fixed);
+//	cout.setf(ios::showpoint);
+//	cout.precision(2);
+//	// Asking the user to input student average
+//	double studentAverage;
+//	cout << "Please enter student1 average: ";
+//	cin >> studentAverage;
+//	cout << "Student1 grade is: " << qualityPoints(studentAverage) << endl;
+//	cout << endl;
+//	cout << "----------------------------------------" << endl;
+//
+//
+//	// Passing an existing array to the function and calculate the average of the marks already given
+//	int studentMarks[] = { 77,88,100,97,90 };
+//	cout <<"Student2 grade is: " << qualityPoints(studentMarks) << endl;
+//
+//	return 0;
+//}
+//double qualityPoints(double average) {
+//	int grade;
+//	if (average >= 90)
+//		grade = 4;
+//	else if (average >= 80)
+//		grade = 3;
+//	else if (average >= 70)
+//		grade = 2;
+//	else if (average >= 60)
+//		grade = 1;
+//	else
+//		grade = 0;
+//
+//	return grade;
+//}
+//
+//double qualityPoints(int arr[]) {
+//	double total = 0;
+//	double  average;
+//	double grade;
+//	int arrLength = sizeof(arr);
+//
+//	cout << "Student2 marks are: " << endl;
+//	for (unsigned int i = 0; i <= arrLength; i++) {
+//		cout << i + 1 << "-" << arr[i] << endl;
+//		total = total + arr[i];
+//	}
+//	average = total / (arrLength + 1);
+//	cout << "Student2 average = " << average << endl;
+//	if (average >= 90)
+//		grade = 4;
+//	else if (average >= 80)
+//		grade = 3;
+//	else if (average >= 70)
+//		grade = 2;
+//	else if (average >= 60)
+//		grade = 1;
+//	else
+//		grade = 0;
+//	
+//	return grade;
+//}
